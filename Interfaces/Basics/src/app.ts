9bcfed341aadef10c48b01583b8c074c6345fce3
RafalDziuba <dziuba.rafal98@gmail.com>
2023-01-17 21:31:54 +0100

    Interfaces: inheritance

--- Interfaces/Basics/src/app.ts
+++ Interfaces/Basics/src/app.ts
@@ -1,20 +1,14 @@
 class Department {
-
-
   // private name: string;
   // private id: string;
 
-
   //table, which contains only string values
   private employees: string[] = [];
-
-
 
   // constructor(n: string, id: string) {
   //   this.name = n;
   //   this.id = id;
   // }
-
 
   //SHORT INITIALIZATION
   // with below line it's not necessery to init variables "directly in class", we can
@@ -38,11 +32,19 @@ class Department {
   }
 }
 
+class ITDepartment extends Department {
+  // admins: string[];
+  constructor(id: string, public admins: string[]) {
+    //super keyword execute/call constructor from base class
+    super('IT', id);
+  }
+}
+
 const accounting = new Department('Accounting', 'd1');
+const it = new ITDepartment('d2', ['Me', 'Also Me'])
 
 accounting.addEmployee('The Guy');
-accounting.addEmployee('The Girl');
-
+it.addEmployee('The Girl');
 //adding employee without addEmployee function
 // accounting.employees[2] = 'The next one'
 // ... and it's not working, if we add 'private' before employees table initialization !!!
@@ -52,4 +54,7 @@ accounting.printEmployeeInfo();
 
 // const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
 
+console.log(it);
 // accountingCopy.describe();
+
+// TO DO: ADD ACCOUNTING DEPARTMENT
